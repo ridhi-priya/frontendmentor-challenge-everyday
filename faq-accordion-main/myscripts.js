5df64a3cd456ea14fb035b37aa4c5b8b6ddaa67a
Priya <Ridhi.Priya@shell.com>
2024-11-19 21:26:12 +0530

    added js code

--- faq-accordion-main/myscripts.js
+++
@@ -1,4 +0,0 @@
-// script.js
-
-function myFunction() {
-    alert("Button was clicked!");}
---
+++ faq-accordion-main/script.js
@@ -0,0 +1,47 @@
+// script.js
+
+// function myFunction() {
+//     alert("Button was clicked!");
+// }
+
+
+function myFunction1() {
+    const addBtn=document.querySelector('.add')
+    const pTag1 = document.querySelector('.p1');
+    if (pTag1.style.display === 'none') {
+      pTag1.style.display = 'block';
+      
+    } else {
+      pTag1.style.display = 'none';
+    }
+  }
+  function myFunction2() {
+    // const addBtn=document.getElementById('add')
+    const pTag2 = document.querySelector('.p2');
+    if (pTag2.style.display === 'none') {
+      pTag2.style.display = 'block';
+      
+    } else {
+      pTag2.style.display = 'none';
+    }
+  }
+  function myFunction3() {
+    // const addBtn=document.getElementById('add')
+    const pTag3 = document.querySelector('.p3');
+    if (pTag3.style.display === 'none') {
+      pTag3.style.display = 'block';
+      
+    } else {
+      pTag3.style.display = 'none';
+    }
+  }
+  function myFunction4() {
+    // const addBtn=document.getElementById('add')
+    const pTag4 = document.querySelector('.p4');
+    if (pTag4.style.display === 'none') {
+      pTag4.style.display = 'block';
+      
+    } else {
+      pTag4.style.display = 'none';
+    }
+  }
